729391a06b6c56cba7c88e3936c7d058ad99aa4a
Zhang Peng <atlantis1024@163.com>
2017-08-16 13:57:24 +0800

    :rocket: 添加部署脚本

---
+++ bin/publish.bat
@@ -0,0 +1,8 @@
+@echo off
+echo [INFO] Publis _book to gh-pages.
+cd ../_book
+git init
+git commit --allow-empty -m "update book"
+git checkout -b gh-pages && git add .
+git commit -am "update book"
+git push git@github.com:atlantis1024/gitbook-notes gh-pages --force"
