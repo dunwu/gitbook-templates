b30fffacac7c77cedc1febc77619b1f0273e55d6
Zhang Peng <forbreak@163.com>
2017-10-11 15:49:40 +0800

    :pen: 修改 github 账户名

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -5,4 +5,4 @@ git init
 git commit --allow-empty -m "update book"
 git checkout -b gh-pages && git add .
 git commit -am "update book"
-git push git@github.com:zp1024/gitbook-notes gh-pages --force"
+git push git@github.com:dunwu/gitbook-notes gh-pages --force"
